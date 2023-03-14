18b9b2a45d6dba26364a76179d982250077c66c8
Муминова Мадина <madi92@mail.ru>
2023-03-14 16:20:49 +0300

    fixed app

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,9 +1,15 @@
-from django.http import HttpResponse
+from http import HTTPStatus
+
+from django.http import HttpResponse, response
 
 
 def index(request):
+    if request == 'Get':
+        return response.status_code, HTTPStatus.OK
     return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
+    if request == 'Get':
+        return response.status_code, HTTPStatus.OK
     return HttpResponse('А это вторая страница!')
