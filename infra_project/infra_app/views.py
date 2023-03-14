c944fed069c359bac954c0abe3019461cc416d58
Муминова Мадина <madi92@mail.ru>
2023-03-14 16:53:37 +0300

    /

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -3,9 +3,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, '/', HttpResponse('У меня получилось!'))
+    return HttpResponse('У меня получилось!')
 
 
 def second_page(request):
-    return render(request, '/second_page/',
-                  HttpResponse('А это вторая страница!'))
+    return HttpResponse('А это вторая страница!')
